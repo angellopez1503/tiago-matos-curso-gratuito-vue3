cccb30c0d6ec35887001a2bbac9ab6d498e03c99
Angel Lopez <angellopez15luisa@gmail.com>
2022-10-03 20:55:57 -0500

    8.Enlace de datos bidireccional

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,45 @@
 <template>
-  <h1 :class="{ title: true, 'title-home': isHome }">Curso Vue3</h1>
-  <p :class="pClass">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores,
-    temporibus.
-  </p>
-  <p :style="styleClass">
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem ab eligendi
-    dignissimos dolores illum debitis soluta perferendis mollitia sit illo?
-  </p>
-  <p :class="['btn',flag ? 'btn-success' : 'btn-danger',flag ? 'fw-bold' :'fw-normal', flag ? 'fs-1' : 'fs-6']">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut dolorum vel
-    atque tenetur tempore magni quae et ipsum dolores earum.
-  </p>
-  <div v-for="(obj, index) in todos" :key="obj.id" class="todos-item">
-    <img v-if="obj.imgSrc" :src="obj.imgSrc" width="500" />
-    {{ index + 1 }} - {{ obj.title }}
+  <div></div>
+  <input type="text" v-model="name" />
+  <br />
+  {{ name }}
+  <br />
+  <br>
+  <div>
+    <select name="" id="" v-model="sports">
+      <option value="futbol">Futbol</option>
+      <option value="skate">Skate</option>
+      <option value="tenis">Tenis</option></select
+    ><br />
+    {{ sports }}
   </div>
+  <br /><br />
+  <div>
+    <label for="">News letter</label><br />
+    <input type="radio" value="Sim" v-model="newsletter" />Sim
+    <input type="radio" value="Ñao" v-model="newsletter" />Ñao
+    <br>
+    {{newsletter}}
+  </div>
+<br>
+<br>
+<div>
+  <label for="">Contrato</label><br />
+  <input type="checkbox" v-model="contract" />Acpeta nuestros terminos
+  
+  <br>
+  {{contract}}
+</div>
+<br>
+<br>
+<div>
+  <label for="">Varios checkbox</label><br />
+  <input type="checkbox" value="Azul" v-model="colors" />Azul
+  <input type="checkbox" value="Amarillo" v-model="colors" />Amarillo
+  <br>
+  {{colors}}
+</div>
+
 </template>
 
 <script>
@@ -24,75 +48,17 @@ export default {
   components: {},
   data() {
     return {
-      flag:true,
-      isHome: true,
-      classVar: "",
-      pClass: ["text", "text-home"],
-      styleClass: { color: "aqua", "background-color": "black" },
-      todos: [
-        {
-          userId: 1,
-          id: 1,
-          title: "delectus aut autem",
-          completed: false,
-          imgSrc:
-            "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg",
-        },
-        {
-          userId: 1,
-          id: 2,
-          title: "quis ut nam facilis et officia qui",
-          completed: false,
-          imgSrc:
-            "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg",
-        },
-        {
-          userId: 1,
-          id: 3,
-          title: "fugiat veniam minus",
-          completed: false,
-        },
-        {
-          userId: 1,
-          id: 4,
-          title: "et porro tempora",
-          completed: true,
-        },
-        {
-          userId: 1,
-          id: 5,
-          title:
-            "laboriosam mollitia et enim quasi adipisci quia provident illum",
-          completed: false,
-        },
-      ],
+      name: "Angel",
+      sports: "futbol",
+      newsletter:'',
+      contract:true,
+      colors:[]
     };
   },
 };
 </script>
 
 <style scoped>
-.title {
-  font-size: 20px;
-  color: blue;
-}
-.title-home {
-  font-size: 40px;
-  color: green;
-}
-.text {
-  color: yellow;
-}
-
-.text-home {
-  color: #42b983;
-}
-.todos-item {
-  background: #000;
-  margin: 0 0 5px 0;
-  padding: 3px 6px;
-  color: #fff;
-}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
