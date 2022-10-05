60f7b3c0bb499cc5703350d12cd24e253f4d649c
Angel Lopez <angellopez15luisa@gmail.com>
2022-10-05 05:55:53 -0500

    17.Introduccion a vue Router parte1

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,41 @@
 <template>
-  <div class="container">
-    <BaseAlert :variant="variant" :text="text" @close="onClose" v-if="showAlert" />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link :to="rutaDinamica">Servicios</router-link>
   </div>
+  <router-view/>
 </template>
 
 <script>
-import BaseAlert from "./components/BaseAlert.vue";
-export default {
-  name: "App",
-  components: {
-    BaseAlert
-},
-  data() {
-    return {
-      showAlert:true,
-      text:'El formulario fue enviado',
-      variant:'success'
-    };
-  },
-  methods:{
-    onClose(){
-      console.log("on close")
-      this.showAlert = false
+  export default {
+    data(){
+      return{
+         rutaDinamica:{name:'servicios'}
+      }
     }
   }
-};
 </script>
 
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/ServiciosView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        servicios
+    </div>
+</template>
